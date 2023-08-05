0194f94f9253bb0899e618ee5220457883c0ecc6
pranavmakam7 <makampranav7@gmail.com>
2023-08-05 14:49:30 -0500

    Update

--- p.py
+++ p.py
@@ -1,1 +1,12 @@
-print(" ADF_eficens")
+def cube(y):
+	print(f"Finding cube of number:{y}")
+	return y * y * y
+
+
+lambda_cube = lambda num: num ** 3
+
+# invoking simple function
+print("invoking function defined with def keyword:")
+print(cube(30))
+# invoking lambda function
+print("invoking lambda function:", lambda_cube(30))
